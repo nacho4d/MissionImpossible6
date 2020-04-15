20ef7b8265e481fc779074f322d17317fba7bde3
Guillermo Ignacio Enriquez Gutierrez <ignacio@jp.ibm.com>
2020-04-15 17:52:55 +0900

    Remove all new stuff

--- MissionImpossible/ViewController.m
+++ MissionImpossible/ViewController.m
@@ -16,8 +16,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.view.backgroundColor = UIColor.greenColor;
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 100, self.view.frame.size.width, 44)];
+    label.textColor = UIColor.blackColor;
+    label.text = @"IPHONEOS_DEPLOYMENT_TARGET = 6.0;\n If you see this it works.";
+    label.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:label];
 }
-
 
 @end
