82c424a20a2ab49679bbe20e70b13a41d1c6d84d
Guillermo Ignacio Enriquez Gutierrez <ignacio@jp.ibm.com>
2020-04-15 19:13:19 +0900

    Solve Launch image problems

--- MissionImpossible/ViewController.m
+++ MissionImpossible/ViewController.m
@@ -16,10 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = UIColor.greenColor;
+    self.view.backgroundColor = UIColor.whiteColor;
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 100, self.view.frame.size.width, 44)];
     label.textColor = UIColor.blackColor;
-    label.text = @"IPHONEOS_DEPLOYMENT_TARGET = 6.0;\n If you see this it works.";
+    label.text = @"IPHONEOS_DEPLOYMENT_TARGET = 6.0;\n If you see this in iOS6 it works.";
     label.textAlignment = NSTextAlignmentCenter;
     [self.view addSubview:label];
 }
